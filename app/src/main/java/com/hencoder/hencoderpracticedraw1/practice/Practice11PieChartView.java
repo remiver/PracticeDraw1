3f3ee89a12ed2ac80408b7d25b83695a7ef824cb
Jayl <revu@qq.com>
2017-11-20 14:18:14 +0800

    partial commit for q11

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice11PieChartView extends View {
+    // pie constants
+    private static final int CHART_CENTER_X = 430;
+    private static final int CHART_CENTER_Y = 300;
+    private static final int CHART_RADIUS = 270;
 
     public Practice11PieChartView(Context context) {
         super(context);
@@ -26,5 +31,9 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+    }
+
+    private void drawPie(Canvas canvas, int color, float percent, String desc) {
+
     }
 }
