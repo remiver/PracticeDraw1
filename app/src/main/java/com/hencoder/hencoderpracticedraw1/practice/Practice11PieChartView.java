c1d4f4e2e0a2da53802d820e023fed763d03e9cf
Jayl <revu@qq.com>
2017-11-26 22:28:53 +0800

    solve q11

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -9,9 +9,22 @@ import android.view.View;
 
 public class Practice11PieChartView extends View {
     // pie constants
-    private static final int CHART_CENTER_X = 430;
-    private static final int CHART_CENTER_Y = 300;
-    private static final int CHART_RADIUS = 270;
+    private static final int CHART_BOUNDRY_LEFT = 160;
+    private static final int CHART_BOUNDRY_TOP = 30;
+    private static final int CHART_BOUNDRY_RIGHT = 700;
+    private static final int CHART_BOUNDRY_BOTTOM = 570;
+    private static final int CHART_PIE_GAP = 2;
+
+    // pie data
+    private ChartPie[] arrPie = new ChartPie[]{
+            new ChartPie("Froyo", 0.005f, 0xFF27FF4E),
+            new ChartPie("Gingerbread", 0.02f, 0xFF9C27B0),
+            new ChartPie("Ice Cream Sandwich", 0.02f, 0xFF9E9E9E),
+            new ChartPie("Jelly Bean", 0.15f, 0xFF009688),
+            new ChartPie("KitKat", 0.3f, 0xFF2196F3),
+            new ChartPie("Lolipop", 0.35f, 0xFFF44336),
+            new ChartPie("MarshMallow", 0.15f, 0xFFFFC107)
+    };
 
     public Practice11PieChartView(Context context) {
         super(context);
@@ -31,9 +44,39 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+        for (ChartPie pie : arrPie) {
+            drawPie(canvas, pie.color, pie.percent, pie.name, pie.name.equals("Lolipop"));
+        }
     }
 
-    private void drawPie(Canvas canvas, int color, float percent, String desc) {
+    // start degree for drawing pie
+    private int mStartDegree = 0;
 
+    private void drawPie(Canvas canvas, int color, float percent, String desc, boolean selected) {
+        // init paint
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(color);
+
+        int sweepAngel = (int) (360 * percent) - CHART_PIE_GAP;
+
+        int transform = selected ? -20 : 0;
+
+        canvas.drawArc(CHART_BOUNDRY_LEFT + transform, CHART_BOUNDRY_TOP + transform,
+                CHART_BOUNDRY_RIGHT + transform, CHART_BOUNDRY_BOTTOM + transform,
+                mStartDegree, sweepAngel, true, paint);
+
+        mStartDegree += sweepAngel + CHART_PIE_GAP;
+    }
+
+    private class ChartPie {
+        public String name;
+        public float percent;
+        public int color;
+
+        public ChartPie(String name, float percent, int color) {
+            this.name = name;
+            this.percent = percent;
+            this.color = color;
+        }
     }
 }
